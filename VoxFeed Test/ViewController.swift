78e19018a8de7f78dcc063b8b5e2764d0fff142c
Mario <albeto8@gmail.com>
2019-02-19 12:05:40 -0600

    Initial Commit

---
+++ VoxFeed Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoxFeed Test
+//
+//  Created by Mario  on 2/19/19.
+//  Copyright © 2019 Mario . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
